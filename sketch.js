fb5cba8f810e6eab53339ef5b1fd6ebe8452f733
Denis <denis.o.shapiro@gmail.com>
2019-05-15 21:11:56 -0400

    added reset button and randomized colors

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//const div = document.createElement('div');
+let resetButton = document.getElementById("reset");
 let numBoxes = 16;
 
 createGridDiv(numBoxes);
@@ -15,6 +15,22 @@ function createGridDiv(boxesPerSide){
 
 function chooseColor(){
     if(this.style.backgroundColor == "white"){
-        this.style.backgroundColor = "red";
+        this.style.backgroundColor = randomColor();
     }
 }
+
+function randomColor(){
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+resetButton.addEventListener('click', () => {
+    const divs = document.querySelectorAll(".box");
+    divs.forEach(box => {
+        box.style.backgroundColor = 'white';
+    });
+});
+
+
+
+
+
